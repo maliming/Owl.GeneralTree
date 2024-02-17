222cfedd93a8b1c3e90eecef45b9ff2304002d65
maliming <malimings@gmail.com>
2024-02-17 14:53:10 +0800

    Update to abp 8.0.

--- test/Owl.GeneralTree.MongoDB.Tests/MongoDB/GeneralTreeManager_Tests.cs
+++ test/Owl.GeneralTree.MongoDB.Tests/MongoDB/GeneralTreeManager_Tests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Owl.GeneralTree.MongoDB;
 
+[Collection(MongoTestCollection.Name)]
 public class GeneralTreeManager_Tests : GeneralTreeManager_Tests<GeneralTreeMongoDbTestModule>
 {
 }
--- test/Owl.GeneralTree.MongoDB.Tests/MongoDB/GeneralTreeMongoDbTestModule.cs
+++ test/Owl.GeneralTree.MongoDB.Tests/MongoDB/GeneralTreeMongoDbTestModule.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Mongo2Go;
-using Volo.Abp;
-using Volo.Abp.Data;
+﻿using Volo.Abp.Data;
 using Volo.Abp.Modularity;
 
 namespace Owl.GeneralTree.MongoDB;
@@ -12,22 +9,11 @@ namespace Owl.GeneralTree.MongoDB;
 )]
 public class GeneralTreeMongoDbTestModule : AbpModule
 {
-    private readonly MongoDbRunner _mongoDbRunner = MongoDbRunner.Start();
-
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        var connectionString = _mongoDbRunner.ConnectionString.EnsureEndsWith('/') +
-                               "Db_" +
-                               Guid.NewGuid().ToString("N");
-
         Configure<AbpDbConnectionOptions>(options =>
         {
-            options.ConnectionStrings.Default = connectionString;
+            options.ConnectionStrings.Default = MongoDbFixture.GetRandomConnectionString();
         });
-    }
-
-    public override void OnApplicationShutdown(ApplicationShutdownContext context)
-    {
-        _mongoDbRunner.Dispose();
     }
 }
